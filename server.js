8bc156153348c442689dc682cc089003a7854ab4
Philip Scott <pscott@zeptohost.com>
2018-12-13 22:49:28 -0500

    add ping handler

--- server.js
+++ server.js
@@ -5,14 +5,21 @@ const colyseus = require('colyseus')
 const RelayRoom = require('./room')
 const http = require('http')
 
-const port = process.env.PORT || config.app.port
-const server = http.createServer()
-const roomServer = new colyseus.Server({ server })
-
-roomServer.register('relay', RelayRoom)
-
-server.listen(port, err => {
+// Add an HTTP ping handler to avoid Heroku timeout
+const onPing = (_, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  res.write('hello, world')
+  res.end()
+}
+const onListen = port => err => {
   if (err) throw err
 
   console.log(`Server is listening on port ${port}`)
-})
+}
+
+const port = process.env.PORT || config.app.port
+const server = http.createServer(onPing)
+const roomServer = new colyseus.Server({ server })
+
+roomServer.register('relay', RelayRoom)
+server.listen(port, onListen(port))
